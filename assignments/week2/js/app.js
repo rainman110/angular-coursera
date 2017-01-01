7a8d812217d391c0f32f0fb1e36cc2bec301338b
Martin Siggel <martin.siggel@dlr.de>
2017-01-01 17:17:22 +0100

    implemented solution to week2 assignment

---
+++ assignments/week2/js/app.js
@@ -0,0 +1,63 @@
+(function() {
+
+angular.module("ShoppingListCheckOff", [])
+
+.controller("ToBuyController", ToBuyController)
+.controller("AlreadyBoughtController", AlreadyBoughtController)
+.service("ShoppingListCheckOffService", ShoppingListCheckOffService)
+;
+
+
+AlreadyBoughtController.$inject = ["ShoppingListCheckOffService"];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+    this.getItems = function() {
+        return ShoppingListCheckOffService.getBought();
+    }
+}
+
+ToBuyController.$inject = ["ShoppingListCheckOffService"];
+function ToBuyController(ShoppingListCheckOffService) {
+    this.getItems = function() {
+        return ShoppingListCheckOffService.getToBuy();
+    }
+
+    this.markBought = function(idx) {
+        return ShoppingListCheckOffService.markBought(idx);
+    }
+}
+
+function ShoppingListCheckOffService() {
+
+    // pre.populate list
+    this.itemsToBuy = [
+        { name: "cookies", quantity: 2 },
+        { name: "chips", quantity: 3 },
+        { name: "potatos", quantity: 20 },
+        { name: "apples", quantity: 4 },
+        { name: "toothpaste", quantity: 1 }
+    ];
+
+    this.itemsBought = [];
+
+    this.getToBuy = function () {
+        return this.itemsToBuy;
+    };
+
+    this.getBought = function () {
+        return this.itemsBought;
+    }
+
+    this.markBought = function (idx) {
+        var item = this.itemsToBuy[idx];
+
+        // remove from toBuy list
+        this.itemsToBuy.splice(idx, 1);
+
+        // add to itemsBought list
+        this.itemsBought.push(item);
+    }
+
+
+}
+
+})();
